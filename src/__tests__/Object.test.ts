acf70f44a59853b32d0483cef44b42468bf47cf8
xpa <xpadev@gmail.com>
2023-02-22 13:51:10 +0900

    improve: add test

--- src/__tests__/Object.test.ts
+++ src/__tests__/Object.test.ts
@@ -13,3 +13,12 @@ test("getSlot", () => {
 test("clone", () => {
   expect(run("hoge={test:256};huga=hoge.clone;huga.test=1024;return hoge.test")).toBe(256);
 });
+
+test("self", () => {
+  expect(run("obj=Object.clone;obj.def(test(),self.hoge=1);obj.test();return obj.hoge")).toBe(1);
+  expect(
+    run(
+      "obj=Object.clone;obj.def(test(),self.hoge=1);obj2=Object.clone;obj2.def(test(),self.hoge=obj.clone;hoge.test());obj2.test();return obj2.hoge.hoge",
+    ),
+  ).toBe(1);
+});
