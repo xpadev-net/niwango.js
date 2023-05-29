ccb2d0e4f176fb2408ef932b8d316a27b45a895d
xpa <xpadev@gmail.com>
2023-05-30 03:30:20 +0900

    improve: add timestamp to banner

--- rollup.config.js
+++ rollup.config.js
@@ -10,6 +10,8 @@ const banner = `/*!
   niwango.js v${pkg.version}
   (c) 2023 xpadev-net https://xpadev.net
   Released under the ${pkg.license} License.
+  
+  build at: ${(new Date()).getTime()}
 */`;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
