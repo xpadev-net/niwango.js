04274b10ed27b9c354be8d5855dff967a2dfe9e0
xpa <xpadev@gmail.com>
2023-06-06 18:20:19 +0900

    fix: script

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,1 +1,3 @@
-document.body.innerHTML = `<div style="position: absolute;top: 50%;left: 50%;transform: translate(-50%,-50%);font-size: 50px;color: #fff;background: rgba(0,0,0,0.5);">最新版への更新をお願いします</div>`
+window.onload = () => {
+  document.body.innerHTML = `<div style="position: absolute;top: 50%;left: 50%;transform: translate(-50%,-50%);font-size: 50px;color: #fff;background: rgba(0,0,0,0.5);">最新版への更新をお願いします</div>`
+}
