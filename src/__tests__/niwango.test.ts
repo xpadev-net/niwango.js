0a2865a95e9e84ed4f5b2fa21d80b2ea155b55fa
xpadev <xpadev@gmail.com>
2023-10-24 09:29:16 +0900

    fix: tests

--- src/__tests__/niwango.test.ts
+++ src/__tests__/niwango.test.ts
@@ -1,8 +1,10 @@
 import { run } from "@/testUtils";
 
 test("rand", () => {
-  const rand1 = run(`return rand("hoge")`);
-  const rand2 = run(`return rand("hoge")`);
+  const rand1 = run(`rand("hoge")`);
+  const rand2 = run(`rand("hoge")`);
+  const rand3 = run(`rand("huga")`);
   expect(rand1).toBe(rand2);
+  expect(rand3).not.toBe(rand1);
 });
 export {};
